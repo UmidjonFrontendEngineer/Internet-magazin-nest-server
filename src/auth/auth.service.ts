b051907ca88fab820ec14c30f223f6d32f6c8a7b
umidjon sharipov <umidjonsharipov283@gmail.com>
2026-07-25 20:15:26 +0500

    form data import writed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, UnauthorizedException, BadRequestException } from '@nestjs/
 import { JwtService } from '@nestjs/jwt';
 import { DatabaseService } from '../database/database.service';
 import * as bcrypt from 'bcrypt';
-import * as FormData from 'form-data';
+import FormData from 'form-data';
 const IMGBB_API_KEY = process.env.IMGBB_API_KEY
 
 @Injectable()
