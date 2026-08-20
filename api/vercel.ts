24dd2097dd73069ce37c3bc1f0d2ea8a3647d3df
umidjon sharipov <umidjonsharipov283@gmail.com>
2026-08-20 18:35:04 +0500

    Add vercel configuration and api entry point

---
+++ api/vercel.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Server } from 'http';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const expressApp = express();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  await app.init();
+}
+
+bootstrap();
+
+export default expressApp;
--- src/workers/dto/create-worker.dto.ts
+++ src/workers/dto/create-worker.dto.ts
@@ -11,7 +11,7 @@ export class CreateWorkerDto {
 
     @IsOptional()
     @IsString({ message: "Rol matn shaklida bo'lishi kerak" })
-    role?: string;
+    role!: string;
 
     @IsNotEmpty()
     @IsUUID()
