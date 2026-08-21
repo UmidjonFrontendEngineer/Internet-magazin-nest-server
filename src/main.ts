de237f43df00c0664f8d24778f5d873fcf50d85d
umidjon sharipov <umidjonsharipov283@gmail.com>
2026-08-21 10:35:16 +0500

    origin: true

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization, X-Requested-With',
+  });
 
   app.use(express.json({ limit: '10mb' }));
   app.use(express.urlencoded({ limit: '10mb', extended: true }));
