0e9610f75574d47fecd33a10653163c9d9d2e143
umidjon sharipov <umidjonsharipov283@gmail.com>
2026-08-21 13:26:25 +0500

    updated

--- src/warehouses/warehouses.module.ts
+++ src/warehouses/warehouses.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { WarehousesController } from './warehouses.controller';
 import { WarehousesService } from './warehouses.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+    imports: [
+        JwtModule.register({
+            secret: process.env.JWT_SECRET || 'super-secret-key',
+        }),
+    ],
     controllers: [WarehousesController],
     providers: [WarehousesService],
 })
